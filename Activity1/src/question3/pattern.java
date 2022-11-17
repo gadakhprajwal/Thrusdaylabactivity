6a145fb2cec26e57387b85d7a9dadb2b0bb552b6
prajwalgadakh <prajwalgadakh@localhost>
2022-11-17 17:56:11 +0530

    Activity, 17/Nov/2022

---
+++ Activity1/src/question3/pattern.java
@@ -0,0 +1,14 @@
+package question3;
+
+public class pattern {
+	public static void main (String args[]) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 15; j++) {
+				System.out.println(j);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
